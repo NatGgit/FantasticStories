4dc2169ae5562d1a3fa6b1d49828af1327c4bb55
Natalia <n.grzywalska@gmail.com>
2019-03-16 15:56:35 +0100

    Add Variables and Types files to Tutorials

---
+++ tutorials/VariablesAndTypes.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class VariablesAndTypes {
+    public static void main(String[] args) {
+
+        int b = 3110;
+        char c = 'w';
+        byte d = 0;
+        char e = 'r';
+        short f = 1;
+        char g = 'd';
+        float h = 2.0f;
+        boolean i = true;
+
+        String output = "H" + b + " " + c + d + e + f + g + " " + h + " " + i;
+
+        System.out.println(output);
+    }
+}
